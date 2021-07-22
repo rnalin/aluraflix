1d5cbf772aab51f8a4f1c46d9f5a2b1cbc90a839
Renan Nalin <nalin.renan@gmail.com>
2021-07-22 15:16:24 -0300

    Implementação do método POST do modelo Video.

--- src/main/java/com/renius/AluraFlix/AluraFlixApplication.java
+++ src/main/java/com/renius/AluraFlix/AluraFlixApplication.java
@@ -1,13 +1,31 @@
 package com.renius.AluraFlix;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.renius.AluraFlix.model.Video;
+import com.renius.AluraFlix.repositories.VideoRepository;
+
 @SpringBootApplication
-public class AluraFlixApplication {
+public class AluraFlixApplication implements CommandLineRunner{
+	@Autowired
+	private VideoRepository videoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AluraFlixApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Video video1 = new Video(null, "Mozart Symphony No. 40 - 10 Hours", "The famous Mozart symphony no. 40 extended to full 10 Hours!","https://www.youtube.com/watch?v=EVZtEty8DQM" );
+		Video video2 = new Video(null, "J.S. Bach: 6 Suites for Cello Solo (Full Album) played by István Várdai", "Young Hungarian cellist István Várdai is winner of the Geneva Competition, 3rd prize Tchaikovsky Competition 2007 and first prize winner of the prestigious ARD competition in 2014. ","https://www.youtube.com/watch?v=D83cMncj_Ig" );
+		Video video3 = new Video(null, "Mozart: Eine kleine Nachtmusik - Concertgebouw Kamerorkest - Live Concert - HD", "Mozart: Eine kleine Nachtmusik in G major, KV 525 - Concertgebouw Chamber Orchestra Recorded the 14th of April 2013 - Concertgebouw Amsterdam","https://www.youtube.com/watch?v=QZWKUszkbXU" );
+		
+		videoRepository.saveAll(Arrays.asList(video1, video2, video3));
 	}
 
 }
--- src/main/java/com/renius/AluraFlix/controllers/VideoController.java
+++ src/main/java/com/renius/AluraFlix/controllers/VideoController.java
@@ -1,24 +1,33 @@
 package com.renius.AluraFlix.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.renius.AluraFlix.model.Video;
 import com.renius.AluraFlix.services.VideoServices;
 
 @RestController
-@RequestMapping(value = "/video")
+@RequestMapping(value = "/")
 public class VideoController {
 	@Autowired
 	private VideoServices service;
 	
-	@RequestMapping(value="/{id}", method = RequestMethod.GET)
-	public ResponseEntity<?> buscarVideoPorId(@PathVariable Integer id) {
-		Video obj = service.findVideoById(id);
-		return ResponseEntity.ok().body(obj);
+	@GetMapping(value = "/videos")
+	public ResponseEntity<?> getAllVideos(){
+		List<Video> videosList = service.findAllVideos();
+		return ResponseEntity.ok().body(videosList);
 	}
+	
+	@GetMapping(value = "video/{id}")
+	public ResponseEntity<?> getVideosById(@PathVariable Integer id) {
+		Video video = service.findVideoById(id);
+		return ResponseEntity.ok().body(video);
+	}
+	
 }
